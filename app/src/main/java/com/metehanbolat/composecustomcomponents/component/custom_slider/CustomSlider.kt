cf25338800db16208d1945661e3e21915616c494
Metehan <metehan.bolat.ie@gmail.com>
2023-03-09 20:37:02 +0300

    CustomSlider

---
+++ app/src/main/java/com/metehanbolat/composecustomcomponents/component/custom_slider/CustomSlider.kt
@@ -0,0 +1,101 @@
+package com.metehanbolat.composecustomcomponents.component.custom_slider
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Slider
+import androidx.compose.material.SliderColors
+import androidx.compose.material.SliderDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomSlider(
+    dates: List<String>,
+    value: Float,
+    onValueChange: (Int) -> Unit
+) {
+    val (sliderValue, setSliderValue) = remember { mutableStateOf(value) }
+    val drawPadding = with(LocalDensity.current) { 10.dp.toPx() }
+    val textSize = with(LocalDensity.current) { 10.dp.toPx() }
+    val lineHeightDp = 10.dp
+    val lineHeightPx = with(LocalDensity.current) { lineHeightDp.toPx() }
+    val canvasHeight = 50.dp
+    val textPaint = android.graphics.Paint().apply {
+        color = if (isSystemInDarkTheme()) 0xffffffff.toInt() else 0xffff47586B.toInt()
+        textAlign = android.graphics.Paint.Align.CENTER
+        this.textSize = textSize
+    }
+    Box(contentAlignment = Alignment.Center) {
+        Canvas(
+            modifier = Modifier
+                .height(canvasHeight)
+                .fillMaxWidth()
+                .padding(
+                    top = canvasHeight
+                        .div(2)
+                        .minus(lineHeightDp.div(2))
+                )
+        ) {
+            val yStart = 0f
+            val distance = (size.width.minus(2 * drawPadding)).div(dates.size.minus(1))
+            dates.forEachIndexed { index, date ->
+                drawLine(
+                    color = Color.DarkGray,
+                    start = Offset(x = drawPadding + index.times(distance), y = yStart),
+                    end = Offset(x = drawPadding + index.times(distance), y = lineHeightPx)
+                )
+                if (index.rem(2) == 1) {
+                    this.drawContext.canvas.nativeCanvas.drawText(
+                        date,
+                        drawPadding + index.times(distance),
+                        size.height,
+                        textPaint
+                    )
+                }
+            }
+        }
+        Slider(
+            modifier = Modifier
+                .testTag("SLIDER_TAG")
+                .fillMaxWidth(),
+            value = sliderValue,
+            valueRange = 0f..dates.size.minus(1).toFloat(),
+            steps = dates.size.minus(2),
+            colors = customSliderColors(),
+            onValueChange = {
+                setSliderValue(it)
+                onValueChange(it.toInt())
+            })
+    }
+}
+
+@Composable
+private fun customSliderColors(): SliderColors = SliderDefaults.colors(
+    activeTickColor = Color.Transparent,
+    inactiveTickColor = Color.Transparent
+)
+
+@Preview
+@Composable
+fun CustomSliderPreview() {
+    CustomSlider(
+        dates = listOf("1", "2", "3", "4", "5", "6", "7"),
+        value = 10f
+    ) {
+        println("Example: $it")
+    }
+}
